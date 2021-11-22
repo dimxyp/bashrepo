0b597df9f8b51f42b3b368968b1eaf02ceca8e02
dxfullv2 <xypolias.d@outlook.com>
2021-11-22 18:04:16 +0200

    Update esxruleset_del.sh

--- esxruleset_del.sh
+++ esxruleset_del.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #dimitris.xypolias@kyndryl.com 
 #git@github.com:dxfullv2/bashrepo/esxruleset 
-#wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/main/esxruleset.sh | sh
+#wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/main/esxruleset_del.sh | sh
 echo ""
 echo "================================================================="
 echo "================================================================="
