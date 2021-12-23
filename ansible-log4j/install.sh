7414c47a02d7da96e6f2e0b2077011416e5adb21
dxfullv2 <xypolias.d@outlook.com>
2021-12-23 15:35:40 +0200

    Update install.sh

--- ansible-log4j/install.sh
+++ ansible-log4j/install.sh
@@ -1,4 +1,7 @@
 #!/bin/sh 
 #dimitris.xypolias@kyndryl.com  
 #git@github.com:dxfullv2/bashrepo/esxruleset 
-wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/main/esxruleset_del.sh | sh
+#wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/Log4j/ansible-log4j/install.sh | sh
+mkdir /opt/Log4j/
+cd /opt/Log4j/
+git clone -b Log4j --single-branch https://github.com/dxfullv2/bashrepo.git
