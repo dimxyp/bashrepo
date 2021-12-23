33a51d196c486408e687fb2ddeb841acf08b93f6
dxfullv2 <xypolias.d@outlook.com>
2021-12-23 15:54:37 +0200

    Update and rename install3.sh to install4.sh

--- ansible-log4j/install4.sh
+++ ansible-log4j/install4.sh
@@ -3,10 +3,10 @@
 #git@github.com:dxfullv2/bashrepo/esxruleset 
 #wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/Log4j/ansible-log4j/install.sh | sh
 echo ""
-read -n 1 -r -s -p "Are you sure? Press any key to continue..."
+read -n 1 -r -s -p "Are you sure? Press any key to continue..." </dev/tty
 mkdir /opt/Log4j/
 cd /opt/Log4j/
-read -e -p "Please enter playbook location ["/opt/Log4j"] : " -i "/opt/Log4j/" path
+read -e -p "Please enter playbook location ["/opt/Log4j"] : " -i "/opt/Log4j/" path </dev/tty
 git clone -b Log4j https://github.com/dxfullv2/bashrepo.git
 mv /opt/Log4j/bashrepo/ansible-log4j/* $path
 rm /opt/Log4j/bashrepo/* -rf
