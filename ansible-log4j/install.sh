f532c784ef22980e43ec71c27775c02abeabd827
dxfullv2 <xypolias.d@outlook.com>
2021-12-23 15:44:02 +0200

    Update install.sh

--- ansible-log4j/install.sh
+++ ansible-log4j/install.sh
@@ -4,7 +4,7 @@
 #wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/Log4j/ansible-log4j/install.sh | sh
 mkdir /opt/Log4j/
 cd /opt/Log4j/
-git clone -b Log4j --single-branch https://github.com/dxfullv2/bashrepo.git
 read -e -p "Please enter playbook location ["/opt/Log4j"] : " -i "/opt/Log4j/" path
+git clone -b Log4j --single-branch https://github.com/dxfullv2/bashrepo.git
 mv /opt/Log4j/bashrepo/ansible-log4j/* $path
 rm /opt/Log4j/bashrepo/* -rf
