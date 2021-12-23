820c2ef0712e77be2b6c63b5f186598741483d9f
dxfullv2 <xypolias.d@outlook.com>
2021-12-23 15:34:11 +0200

    Create install.sh

---
+++ ansible-log4j/install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh 
+#dimitris.xypolias@kyndryl.com  
+#git@github.com:dxfullv2/bashrepo/esxruleset 
+wget -O - https://raw.githubusercontent.com/dxfullv2/bashrepo/main/esxruleset_del.sh | sh
